5a593d9da3124226d3f9a01ebfba8e2e5bd21c0c
Priyankaadev <priyaaswal79@gmail.com>
2026-03-26 22:20:08 +0530

    frontend: register page

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+import {motion} from "motion/react"
+import { ShoppingBasket } from 'lucide-react'
+
+const Welcome = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen text-center p-6'>
+      <motion.div
+      initial={{opacity: 0, y:-20}}
+      animate={{opacity: 1, y:0}}
+      transition={{duration:1}}
+      >
+        <h1 className='text-4xl md:text-5xl font-extrabold text-green-700 '>
+            Snapcart
+        </h1>
+        <ShoppingBasket />
+      </motion.div>
+    </div>
+  )
+}
+
+export default Welcome
